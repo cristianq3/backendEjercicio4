6fd0ff156628d7a749f05906231812b0066c930d
cristianq3 <cristianq3@gmail.com>
2023-06-28 10:36:17 -0300

    Agregada estructura basica del backend de lista tareas

---
+++ src/routes/app.js
@@ -0,0 +1,5 @@
+const express = require('express'); // requiero el modulo express y lo guardo en una variable
+
+const app = express(); // ejecuto express y eso me devuelve un objeto que lo guardo en la variable app
+
+module.exports = app; // asi se exporta en NODE JS
